be0d34a1574f2a19c851f8b9a0c806421a6be2cb
Nishant Pradhan <nishant.pradhan2060@gmail.com>
2025-04-08 16:59:50 +0545

    file validation changed

--- src/hooks/notes/useFileValidation.ts
+++ src/hooks/notes/useFileValidation.ts
@@ -9,8 +9,9 @@ const useFileValidation = () => {
   ) => {
     const formData: FormData = new FormData();
     for (let i = 0; i < files.length; i++) {
+    
       const fileSizeInMB = Number((files[i].size / (1024 * 1024)).toFixed(2));
-      if (acceptedFileTypes.includes(files[i].type) && fileSizeInMB < 25) {
+      if (acceptedFileTypes.includes(files[i].type) && fileSizeInMB < 5) {
 
         formData.append("file", files[i]);
       } else {
